f6b092725f61806fd57305137681554294cff259
Cedric Montavon <cedric.montavon@epitech.eu>
2018-02-05 10:37:42 +0100

    fdp

---
+++ Server/routes/beacons.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+const databaseScript = require('../server_modules/databaseScript');
+
+router.get('/:id', function(req, res, next) {
+    var id = parseInt(req.params.id);
+
+    if (isNaN(id)) {
+        res.send("Requested ID is not a number");
+    }
+    else {
+        databaseScript.init(function () {
+            databaseScript.connect(function () {
+                databaseScript.query('SELECT * FROM beacons WHERE beacons.Id = ' + id, function (rows) {
+                    databaseScript.end();
+
+                    if (rows.localeCompare("[]")) {
+                        res.send(rows);
+                    } else {
+                        res.send("No beacons with this Id have been found");
+                    }
+                });
+            });
+        });
+    }
+});
+
+module.exports = router;
