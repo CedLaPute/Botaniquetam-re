a0e013c700c388e6ae1692b9cfc204af015dbdab
Cedric Montavon <cedric.montavon@epitech.eu>
2018-02-09 16:02:29 +0100

    tour management

--- Server/routes/tours.js
+++ Server/routes/tours.js
@@ -13,6 +13,51 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.get('/add', function(req, res, next) {
+    var db = req.app.get('db');
+
+    db.query('SELECT plants.Id as PlantId, plants.Name as PlantName FROM plants', function (plantsRow) {
+
+        var plantsjsonarray = JSON.parse(plantsRow);
+        var plantarray = [];
+
+        for (var i = 0; i < plantsjsonarray.length; i++) {
+            var value = plantsjsonarray[i].PlantId + "__" + plantsjsonarray[i].PlantName;
+
+            plantarray.push(value);
+        }
+
+        console.log(plantarray);
+
+        res.render('toursAdd', {title: 'ZHAW Botanic', plants: plantarray});
+    });
+});
+
+router.post('/upload', function(req, res, next) {
+    var name = req.body.nameInput;
+
+    if (!name) {
+        res.end("Error : name is empty");
+    } else {
+        var idChain = "";
+        for (var input in req.body) {
+
+            if (input.localeCompare("nameInput")) {
+                var id = input.split("__")[0];
+
+                idChain += id;
+                idChain += ";";
+            }
+        }
+        idChain = idChain.slice(0, -1);
+
+        var db = req.app.get('db');
+        db.query('INSERT INTO tours (Name, Plants, Pillars) VALUES ("' + name + '", "' + idChain + '", "")', function () {
+            res.end("The data has been uploaded");
+        });
+    }
+});
+
 router.get('/:id', function(req, res, next) {
     var id = parseInt(req.params.id);
 
